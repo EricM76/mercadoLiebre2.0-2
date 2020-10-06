c43e101979e419148db4f941f0a610802aa2b1e5
Eric <menaericdaniel@gmail.com>
2020-10-06 14:08:56 -0300

    actualizacion MAC

--- sitio/src/database/config/config.js
+++ sitio/src/database/config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "mercadoLiebre2.0",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 8889
   },
   "test": {
     "username": "root",
